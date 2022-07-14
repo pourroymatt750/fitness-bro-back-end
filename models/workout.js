92d2e99c9af4ba7af453cf01f4fe3574ecd537f9
Alexandrea <lexhom24@gmail.com>
2022-07-14 16:22:01 -0400

    Add the reviewSchema

--- models/workout.js
+++ models/workout.js
@@ -2,10 +2,19 @@ import mongoose from "mongoose"
 
 const Schema = mongoose.Schema
 
+const reviewSchema = new Schema({
+  rating: {type: Number, min: 1, max: 5, default: 5},
+  comment: {type: String, required: true },
+}, {
+  timestamps: true
+})
+
 const workoutSchema = new Schema({
   name: String,
   description: String,
   reviews: [reviewSchema]
+}, {
+  timestamps: true
 })
 
 const Workout = mongoose.model('Workout', workoutSchema)
