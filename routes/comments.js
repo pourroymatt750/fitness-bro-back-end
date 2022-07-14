53eb8010b028a890f65d44a16506ed7f4c870076
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-14 16:29:15 -0400

    add routes

---
+++ routes/comments.js
@@ -0,0 +1,14 @@
+import { Router } from 'express'
+import * as commentsCtrl from '../controllers/comments.js'
+import { decodeUserFromToken, checkAuth } from '../middleware/auth.js'
+
+const router = Router()
+
+/*---------- Public Routes ----------*/
+
+
+/*---------- Protected Routes ----------*/
+router.use(decodeUserFromToken)
+// router.get('/', checkAuth, commentsCtrl.index)
+
+export { router }
