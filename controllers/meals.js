2029130cba11a16b1986eeacaf7e9eac254df957
Omar Farah <omaarfaarah2019@gmail.com>
2022-07-18 15:45:55 -0400

    add meal

--- controllers/meals.js
+++ controllers/meals.js
@@ -19,42 +19,15 @@ export function mealSearch(req, res) {
   });
 }
 export async function addToCollection(req, res) {
-  // req.body.collectedBy = req.user.profile._id
   try{
     const newMeal = await Meal.create(req.body)
     console.log("newMealId",newMeal._id)
-    // const meal = await Meal.findById(newMeal._id)
-    // console.log("meal",meal)
     await Profile.updateOne(
       { _id: req.user.profile },
       { $push: { meals: newMeal } }
     )
-    // const profile = await Profile.findById(req.user.profile)
-    // // console.log(profile)
-    // profile.meals.push(newMeal)
-    // await profile.save()
-    // console.log("*****ADDMEAL",profile)
     res.json(newMeal)
   }catch(err){
     console.log(err)
   }
-  // console.log("!!^#^&$%",req.body)
-  // Meal.create(req.body)
-  // .then((meal)=> {
-  //   Profile.findById(req.user.profile)
-  //   .then(profile => {
-  //     console.log("meal",meal)
-  //     console.log("^!$#!^",profile)
-  //     profile.meals.push(meal._id)
-  //     profile.save()
-  //     .then(()=>{
-  //       console.log("line33",profile)
-  //       res.json(meal)
-  //     })
-  //   })
-  // })
 }
-// export{
-//   mealSearch,
-//   addToCollection
-// }
