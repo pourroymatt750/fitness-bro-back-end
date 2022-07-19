42549f2c519b433faa61933105946cadfa1e3345
Matthew Pourroy <pourroy.matt@gmail.com>
2022-07-19 19:51:48 -0400

    show dupated

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,47 +1,56 @@
 import { Comment } from "../models/comment.js";
 import { Profile } from "../models/profile.js";
 
+export function create(req, res) {
+  req.body.author = req.user.profile
+  Comment.create(req.body)
+  .then(comment => {
+    Profile.findById(req.params.id)
+    .then(profile => {
+      profile.comments.push(comment._id)
+      profile.save()
+      Profile.findById(profile._id)
+      // .populate('comments')
+      // .populate({path:'meals'})
+      // .populate('workouts')
+      
+      .then(populatedProfile => {
+        res.json(populatedProfile)
+      })
+    })  
+  })
+}
+
 // export function create(req, res) {
 //   req.body.author = req.user.profile
 //   Comment.create(req.body)
-//   .then(comment => {
+//   .then(newComment => {
 //     Profile.findById(req.params.id)
 //     .then(profile => {
-//       profile.comments.push(comment._id)
+//       profile.comments.push(newComment._id)
 //       profile.save()
 //       Profile.findById(profile._id)
 //       .populate('comments')
 //       .populate({path:'meals'})
 //       .populate('workouts')
-      
-//       .then(populatedProfile => {
-//         res.json(populatedProfile)
+//       .then(profile => {
+//         profile.populate('comments')
+//         .then(populatedComment => {
+//           newComment.populate('comment')
+//           .then(populatedProfile => {
+//             res.json([populatedProfile, populatedComment])
+//           })
+//         })
 //       })
 //     })  
 //   })
 // }
 
-export function create(req, res) {
-  req.body.author = req.user.profile
-  Comment.create(req.body)
-  .then(newComment => {
-    Profile.findById(req.params.id)
-    .then(profile => {
-      profile.comments.push(newComment._id)
-      profile.save()
-      Profile.findById(profile._id)
-      .populate('comments')
-      .populate({path:'meals'})
-      .populate('workouts')
-      .then(profile => {
-        profile.populate('comments')
-        .then(populatedComment => {
-          newComment.populate('comment')
-          .then(populatedProfile => {
-            res.json([populatedProfile, populatedComment])
-          })
-        })
-      })
-    })  
-  })
-}
+// export function create(req, res) {
+//   req.body.author = req.user.profile
+//   Comment.create(req.body)
+//   .populate('author')
+//   .then(comment => {
+//     res.json(comment)
+//   })
+// }
